057694dd019c188f4591bbb4e4635391c5770234
THTFT <tihinr@gmail.com>
2026-04-24 12:11:07 +0200

    Errorhander

--- Programs/Taschenrechner/errorhandler.c
+++ Programs/Taschenrechner/errorhandler.c
@@ -1,21 +1,41 @@
 #include <stdbool.h>
 #include "token.h"
 #include "scanner.h"
+#include "display.h"
 
 
 
-
-void printErrorMessage(void)
+void printErrorMessage(int fehlercode)
 {
-
+    switch(fehlercode)
+    {
+    case -1:
+    printStdout("Error MATH_FAIL: Mathematischen-Fehler!");
+    break;
+    case -2:
+    printStdout("Error OTHER_OPERATIONFAIL: Fehler bei Anweisung!");
+    break;
+    case -4: 
+    printStdout("Error ZERODIFF: Es kann nicht durch 0 geteilt werden!");
+    break;
+    case -9:
+    printStdout("Error STACK_UNDERFLOW: Es Muessen Zahlen zum Rechnen verwendet werden");
+    break;
+    case -99:
+    printStdout("Error STACK_OVERFLOW: Zu viele Zaheln");
+    break;
+    default:
+    printStdout("Error Error Error");
+    }
+    
 }
 
 
-void handleError(void)
+void handleError(int fehlercode);
 {
     bool ErrorHandled;
 
-    printErrorMessage(); //einmal vor der Loop
+    printErrorMessage(int fehlercode); //einmal vor der Loop
 
 
     while(!ErrorHandled)
