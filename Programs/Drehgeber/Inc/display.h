2f4784cf6f54a7e51f8afc7162bddc06f7f070d5
THTFT <tihinr@gmail.com>
2026-05-09 12:19:06 +0200

    fertiges display ig

---
+++ Programs/Drehgeber/Inc/display.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void display_init(void);
+
+void display_update(double winkel, double Geschwinigkeit);
+
+#endif
--- Programs/Drehgeber/Src/display.c
+++ Programs/Drehgeber/Src/display.c
@@ -1,16 +1,16 @@
 #include "display.h"
 #include "lcd.h"
 
-void display_init(void)
+void display_init(void) /*schreibt den Standart text aufs Display der konstant so bleibt*/
 {
     lcdSetFont(16);
     lcdGotoXY(0,0);
     lcdPrintS("aktueller Grad des Winkels: ");
     lcdGotoXY(0,60);
-    lcdPrintS("Geschwindikeit: ")
+    lcdPrintS("Geschwindikeit: ");
 }
 
-void display_update(double winkel, double Geschwindikeit)
+void display_update(double winkel, double Geschwindikeit) /* aktuallisiert den Winkel und die Geschwindigkeit*/
 {
     lcdGotoXY(0,30);
     lcdPrintInt((int)winkel);
