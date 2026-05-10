461fa695d80de0f889551aeed2b4403582c50ad6
Timo <tihinr@gmail.com>
2026-05-10 22:33:49 +0200

    header datein von display und fsm

--- Programs/Drehgeber/Inc/display.h
+++ Programs/Drehgeber/Inc/display.h
@@ -1,3 +1,13 @@
+/**
+ ******************************************************************************
+ * @file    display.h
+ * @author  Timo Hinrichs, Emre Hakli
+ * @brief   Statemaschine für den Drehgeber
+ ******************************************************************************
+ */
+
+
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
@@ -7,14 +17,48 @@
 #define VELOCITY_Y 1
 #define VELOCITYX 0
 
+/** 
+********************************************************************************
+* @brief  Initialisiert das Display mit dem Starttext
+* @param void
+* @retval void
+*/
+
 void display_init(void);
 
-void display_update(double winkel, double Geschwinigkeit);
+/** 
+********************************************************************************
+* @brief  Formatiert den Winkelwert zu einem String und setzt den Index vom Anglebuffer auf 0
+* @param Angle
+* @retval void
+*/
 
-void prepareVelocityBuffer(double velocity);
 void prepareAngleBuffer(double Angle);
 
+/** 
+********************************************************************************
+* @brief  Formatiert die Geschwindigkeit zu einem String und setzt den Index vom Velocitybuffer auf 0
+* @param velocity
+* @retval void
+*/
+
+void prepareVelocityBuffer(double velocity);
+
+/** 
+********************************************************************************
+* @brief  Schreibt pro Aufruf genau ein Zeichen aufs Display, zuerst Winkel dann Geschwindigkeit
+* @param void
+* @retval void
+*/
+
 void processLCDUpdate(void);
+
+/** 
+********************************************************************************
+* @brief  Schreibt pro Aufruf genau ein Zeichen aufs Display, zuerst Winkel dann Geschwindigkeit
+* @param void
+* @retval void
+*/
 void processLCDUpdate2(void);
 
 #endif
